19c642ffd5638e68b281371990c43f229e7de1a3
Mohsin0306 <mohsinashraf121519@gmail.com>
2025-11-17 20:21:20 -0800

    Fix navigation logo on home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -38,7 +38,7 @@ const Home = () => {
               <div className="flex items-center space-x-2 sm:space-x-4">
                 <div className="w-8 h-8 sm:w-12 sm:h-12 flex items-center justify-center">
                   <img 
-                    src="/src/assets/images/logo.png" 
+                    src={logo} 
                     alt="Car Vistors Logo" 
                     className="w-full h-full object-contain"
                   />
